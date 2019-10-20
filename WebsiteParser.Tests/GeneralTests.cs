9ca3f99244e52e2d73fc80f073d2ccfa8ac83056
Jan Duniec <jasniec25@gmail.com>
2019-10-20 13:45:10 +0200

    renames and fixes

---
+++ WebsiteParser.Tests/GeneralTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebsiteParser;
+using WebsiteParser.Attributes;
+
+namespace WebParser.Tests
+{
+    [TestClass]
+    public class GeneralTests
+    {
+        [TestMethod]
+        public void Run()
+        {
+            var result = WebContentParser.Parse<Metal>(WebsiteParser.Tests.Properties.Resources.Metal);
+        }
+    }
+
+    class Metal
+    {
+        [Selector(@"#band_stats dl:first-child dd:nth-child(2)")]
+        public string Sth { get; set; }
+    }
+}
--- WebsiteParser/Attributes/ConverterAttribute.cs
+++ WebsiteParser/Attributes/ConverterAttribute.cs
@@ -12,14 +12,9 @@ namespace WebsiteParser.Attributes
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class ConverterAttribute : Attribute
     {
-        public ConverterAttribute(IConverter converter)
-        {
-            ConverterInstance = converter;
-        }
-
         public ConverterAttribute(Type converterType)
         {
-            if (!converterType.IsAssignableFrom(typeof(IConverter)))
+            if (!typeof(IConverter).IsAssignableFrom(converterType))
                 throw new NotSupportedException($"{converterType.Name} is not assignable from {typeof(IConverter).Name}");
             if (!converterType.GetConstructors().Any(c => c.GetParameters().Length == 0))
                 throw new NotSupportedException($"Converter must have a parameterless constructor to use this constructor. You can use another constructor to pass IConverter instance");
